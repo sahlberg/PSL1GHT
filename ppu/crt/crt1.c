1b836ca081c37b9b045be2fdbec431dd5816321a
Damián Parrino <bucanero@users.noreply.github.com>
2020-06-15 20:26:30 -0300

    Add access()

--- ppu/crt/crt1.c
+++ ppu/crt/crt1.c
@@ -33,6 +33,7 @@ extern void __librt_rewinddir_r(struct _reent *r,DIR *dirp);
 extern void __librt_seekdir_r(struct _reent *r,DIR *dirp,long int loc);
 extern int __librt_rmdir_r(struct _reent *r,const char *dirname);
 extern int __librt_unlink_r(struct _reent *r,const char *path);
+extern int __librt_access_r(struct _reent *r,const char *path,int amode);
 
 extern int __librt_usleep_r(struct _reent *r,useconds_t usec);
 extern unsigned int __librt_sleep_r(struct _reent *r,unsigned int seconds);
@@ -78,6 +79,7 @@ static void __syscalls_init(void)
 	__syscalls.seekdir_r = __librt_seekdir_r;
 	__syscalls.rmdir_r = __librt_rmdir_r;
 	__syscalls.unlink_r = __librt_unlink_r;
+	__syscalls.access_r = __librt_access_r;
 
 	__syscalls.sleep_r = __librt_sleep_r;
 	__syscalls.usleep_r = __librt_usleep_r;

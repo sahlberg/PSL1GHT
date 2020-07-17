37da6d650ef98ebc5eb4427db2f843464a130ebd
Mike Wiedenbauer <shagkur@gmx.net>
2020-07-17 09:22:45 +0200

    - improve samples/fix aspect ratio

--- samples/graphics/rsxtest/include/rsxutil.h
+++ samples/graphics/rsxtest/include/rsxutil.h
@@ -2,14 +2,29 @@
 #define __RSXUTIL_H__
 
 #include <ppu-types.h>
+#include <rsx/rsx.h>
 
 #define CB_SIZE		0x100000
 #define HOST_SIZE	(32*1024*1024)
 
+#define FRAME_BUFFER_COUNT					2
+
 extern gcmContextData *context;
+
+extern u32 curr_fb;
+
 extern u32 display_width;
 extern u32 display_height;
-extern u32 curr_fb;
+
+extern u32 depth_pitch;
+extern u32 depth_offset;
+extern u32 *depth_buffer;
+
+extern u32 color_pitch;
+extern u32 color_offset[FRAME_BUFFER_COUNT];
+extern u32 *color_buffer[FRAME_BUFFER_COUNT];
+
+extern f32 aspect_ratio;
 
 void setRenderTarget(u32 index);
 void init_screen(void *host_addr,u32 size);
